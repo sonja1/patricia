1875a3c0d725d014331fc79178fc1a6d87e2292a
sonjacotra <sonjacotra@sonjas-mbp.hitronhub.home>
2020-02-18 13:49:22 -0700

    added constructor (the one with the ints and also a copy constructor), getter methods, and useMove/useItem methods. super rough, just an outline

--- Character.java
+++ Character.java
@@ -1,50 +1,96 @@
 public class Character {
-  private int level = 1;
-  private int hp = 10;
-  private int mp = 10;
-  private int attack = 5;
-  private int defense = 5;
-  private int speed = 5;
-  private final int maxLevel = 10;
-  private int expPoints;
-  // private ArrayList<Moves> moves = new ArrayList<Moves>();
+  private int mp; // index 0
+  private int atk; // index 1
+  private int def; // index 2
+  private int speed; // index 3
+  private int currentHp; // index 4
+  private int maxHp; // should we also have an index for this? if so, index 5
+  private Move[] moves;
   
-  public void increase(String stat, int addition) {
-	  if (stat.equals("hp")) {
-		  if (addition <= expPoints) {
-			  hp += addition;
-			  expPoints -= addition;
-		  }
+  //Constructors
+  
+  public Character(int anHp, int anMp, int anAtk, int aDef, int aSpeed) {
+	  if (anHp > 0) {
+		  maxHp = anHp;
+		  currentHp = anHp;
 	  }
-	  if (stat.equals("mp")) {
-		  if (addition <= expPoints) {
-			  mp += addition;
-			  expPoints -= addition;
-		  }
+	  if (anMp > 0) {
+		  mp = anMp;
 	  }
-	  if (stat.equals("attack")) {
-		  if (addition <= expPoints) {
-			  attack += addition;
-			  expPoints -= addition;
-		  }
+	  if (anAtk > 0) {
+		  atk = anAtk;
 	  }
-	  if (stat.equals("defense")) {
-		  if (addition <= expPoints) {
-			  defense += addition;
-			  expPoints -= addition;
-		  }
+	  if (aDef > 0) {
+		  def = aDef;
+	  }
+	  if (aSpeed > 0) {
+		  speed = aSpeed;
 	  }
   }
   
-  public void levelUp() {
-	  if (level < maxLevel) {
-		  level++;
-		  expPoints = level%2;
-		  this.increase(hp,addition);
-		  
-	  }
-	  
+  //Copy constructor
+  
+  public Character (Character aCharacter) {
+	  this.mp = aCharacter.mp;
+	  this.atk = aCharacter.atk;
+	  this.def = aCharacter.def;
+	  this.speed = aCharacter.speed;
+	  this.currentHp =aCharacter.currentHp;
+	  this.maxHp = aCharacter.maxHp;
+	  this.moves = Character.moves;
   }
+  
+  // Getters 
+  
+  public int getHp() {
+	  return hp;
+  }
+  
+  public int getMaxHp() {
+	  return maxHp;
+  }
+  
+  public int getMp() {
+	  return mp;
+  }
+  
+  public int getAtk() {
+	  return atk;
+  }
+  
+  public int getDef() {
+	  return def;
+  }
+  
+  public int getSpeed() {
+	  return speed;
+  }
+  
+  public Move[] getMoves() {
+	  return moves;
+  }
+  
+  //Methods
+  
+  public void useMove(Move aMove, Character anAttacker) {
+	  currentHp = currentHp - (aMove.getPower()*anAttacker.getAttack())/(def);
+	  // this is super rough but im assuming this method being used on the character is the character getting attacked???
+	  // we can come up with a better formula later tho
+	  // i think there should be a multiplier before the second term that is either 0 or 1 based on whether the attack hits or not
+	  // let me know what y'all think tho lol
+  }
+  
+  public void useItem(Item anItem ) {
+	  if (anItem.consumable() == true) {
+		  mp += 0; // these 0s will be replaced by like idk. something to access the statChanges array
+		  atk += 0; // idk how to do it rn but like. something like atk += anItem.statChanges[1] ??? idk how to write it yet
+		  def += 0; // indices for each stat are listed in comments next to the instance variables
+		  speed += 0;
+		  currentHp += 0;
+	  }
+  }
+  
+  
   
   
   

4e1f14fb74bdc6471f327799aee4f6dd3ab13151
sonjacotra <sonjacotra@sonjas-mbp.hitronhub.home>
2020-02-25 23:26:03 -0700

    finished the player class with levelUp and allotStats methods. started on the enemy class, will finish that tomorrow

--- Player.java
+++ Player.java
@@ -1,3 +1,20 @@
 public class Player extends Character {
+  private int xp = 0;
+  private int level;
   
+  //Methods
+  
+  public void levelUp(int addXp) {
+	  level++;
+	  xp += addXp;
+  }
+  
+  public void allotStats(int[] stats) {
+	  if (stats[0]+stats[1]+stats[2]+stats[3] == xp)
+		  atk += stats[0];
+	      def += stats[1];
+	      speed += stats[2];
+	      maxHp += stats[3];
+	      xp = 0;
+  }
 }

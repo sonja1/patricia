2e0f3725b64ddd7d92cd3910148cb5ac4be0b530
sonjacotra <sonjacotra@localhost>
2020-02-26 22:11:55 -0700

    added test for getCurrentHp()

--- Demo.java
+++ Demo.java
@@ -1,3 +1,13 @@
 public class Demo {
-  
+  public static void main(String[] args) {
+	  Character dummy = new Character("dummy",10,10,10,10);
+	  
+	  //Getting the currentHp
+	  
+	  System.out.println(dummy.getCurrentHp());
+	  
+	  //Move dummyMove = new Move("dummy move",20,1.0, [0,0,0,0], dummy);
+	  
+	  
+  }
 }

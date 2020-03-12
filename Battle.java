c3d273abd1ba9ad57560d7b78b77dd6f4c9abcfd
sonjacotra <sonjacotra@sonjas-mbp.hitronhub.home>
2020-03-12 00:15:49 -0600

    battle constructor based on two characters

--- Battle.java
+++ Battle.java
@@ -1,28 +1,16 @@
-import java.util.Random;
-import java.util.Scanner;
-
 public class Battle {
-  
-	//Constructors but tbh im not sure what else we need for this :/
-	public Battle(Player aPlayer, Enemy anEnemy)) {
-		Move[] usablePlayerMoves = aPlayer.getMoves();
-		Move[] enemyMoves = anEnemy.getMoves();
-		Scanner keyboard = new Scanner(System.in);
-		Random r = new Random();
-		if (aPlayer.getSpeed() > anEnemy.getSpeed()) {
-			while (aPlayer.getCurrentHp() > 0 && anEnemy.CurrentHp() > 0) {
-				System.out.println("Select a move");
-				for (Move aMove : usablePlayerMoves) {
-					if ((keyboard.next()).equals(aMove.getName())) {
-						anEnemy.useMove(aMove);
-					}
-				}
-				if (anEnemy.getCurrentHp() > 0) {
-					Move enemyMove = r.next();
-					aPlayer.useMove(enemyMove); 		
-			    }
-		    }
-	    }
-		
+	private boolean character1sTurn;
+	private Character character1;
+	private Character character2;
+	
+	public Battle(Character aCharacter1, Character aCharacter2) {
+		this.character1 = character1;
+		this.character2 = character2;
+		this.character1sTurn = true;
+		if (aCharacter1.getSpeed() < aCharacter2.getSpeed()) {
+			this.character1sTurn = false;
+		}
 	}
+	
+ 
 }

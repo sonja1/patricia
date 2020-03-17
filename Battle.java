6e7c21da4e2efd30c4ba039cdb6b2d3f144c8819
sonjacotra <sonjacotra@sonjas-mbp.hitronhub.home>
2020-03-17 12:16:05 -0600

    added skeletons for the methods in battle class

--- Battle.java
+++ Battle.java
@@ -1,16 +1,34 @@
 public class Battle {
-	private boolean character1sTurn;
 	private Character character1;
 	private Character character2;
+	private int state;
+	int speed1;
+	int speed2;
 	
 	public Battle(Character aCharacter1, Character aCharacter2) {
 		this.character1 = character1;
 		this.character2 = character2;
-		this.character1sTurn = true;
-		if (aCharacter1.getSpeed() < aCharacter2.getSpeed()) {
-			this.character1sTurn = false;
+		this.speed1 = character1.getSpeed();
+		this.speed2 = character2.getSpeed();
+		this.state = 1;
+		if (speed1 < speed2) {
+			this.state = -1;
 		}
 	}
 	
- 
+	public void initiateBattle() {
+		int one = 1;
+	}
+	
+	public int BattleState() {
+		return 0;
+	}
+	
+	public void characterUseItem(int anItem) {
+		int one = 1;
+	}
+	
+	public void characterUseMove(int aMove) {
+		int one = 1;
+	}
 }

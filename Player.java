acbcd88ca0917b8cf6368708e8f1cb4344743d4d
sonjacotra <sonjacotra@sonjas-mbp.hitronhub.home>
2020-03-30 14:10:11 -0600

    added ability to add move when leveling up at  acertain level

--- Player.java
+++ Player.java
@@ -1,32 +1,41 @@
 public class Player extends Character {
   private int xp = 0;
   private int level;
+  private Move[][] possibleMoves = new Move[][];
   
   //Constructor
   
   public Player(int anXp, int aLevel, String aName, int anHp, int anAtk, int aDef, int aSpeed) {
 	  super(aName, anHp, anAtk, aDef, aSpeed);
 	  if (anXp >= 0) {
-		  xp = anXp;
+		  this.xp = anXp;
 	  }
 	  if (aLevel > 0) {
-		  level = aLevel;
+		  this.level = aLevel;
 	  }
   }
   //Getters
   
   public int getLevel() {
-	  return level;
+	  return this.level;
   }
   
   public int getXp() {
-	  return xp;
+	  return this.xp;
   }
+  
   //Methods
   
   public void levelUp(int addXp) {
-	  level++;
-	  xp += addXp;
+	  this.level++;
+	  this.xp += addXp;
+	  if (this.getLevel() % 3 == 0) {
+		  Move newMove = possibleMoves[this.level][i];
+		  if (super.getKnownMoves() < (super.getMoves()).length) {
+			  super.addMove(newMove);
+		  } else {
+			  super.replaceMove(newMove);
+		  }
   }
   
   public void allotStats(int addAtk, int addDef, int addSpeed, int addHp) {
@@ -38,4 +47,8 @@ public class Player extends Character {
 	      super.setCurrentHp(super.getCurrentHp() + addHp);
 	      xp = 0;
   }
+  
+  
+  }
+   
 }

f00df62eadab0cb4c7276a5f01662401a341ab1f
sonjacotra <sonjacotra@sonjas-mbp.hitronhub.home>
2020-04-05 23:11:06 -0600

    added javadoc comments

--- Enemy.java
+++ Enemy.java
@@ -1,33 +1,49 @@
+/**
+ * Represents an enemy in the game.
+ * Child of character class, also has a tier and a droppable item.
+ */
+
 public class Enemy extends Character {
-  private char[][] appearance; 
+  //private char[][] appearance; 
   private Items droppable;
   private int tier;
   
   //Constructor
-  
+   /**
+    * Creates an enemy with given tier, name, and stats.
+    */
   public Enemy (int aTier, String aName, int anHp, int anAtk, int aDef, int aSpeed) {
 	  super(aName,anHp,anAtk,aDef,aSpeed);
-	  tier = aTier;  
+	  if (aTier == 1 || aTier == 0 || aTier == -1) {
+		  tier = aTier;
+	  }  
   }
   
   
   //Getters
-  
+  /**
+   * Gets the enemy's tier.
+   * @return the enemy's tier.
+   */
   public int getTier() {
 	  return tier;
   }
   
   //Setters
-  
+  /**
+   * Sets the tier of the enemy, must be an integer from -1 to 1.
+   */
   public void setTier(int aTier) {
-	  if (aTier > 0) {
+	  if (aTier == 1 || aTier == 0 || aTier == -1) {
 		  tier = aTier;
 	  }
   }
-  
+   /**
+    * Setting the enemy's stats relative to the player and the tier.
+    */
   public void setStats(Player aPlayer, int aTier) {
 	  int playerTotalStat = aPlayer.getAtk() + aPlayer.getDef() + aPlayer.getSpeed() + aPlayer.getMaxHp();
-	  int enemyTotalStat = (1 + (aTier*playerTotalStat)/10);
+	  int enemyTotalStat = (1 + aTier/10)*playerTotalStat;
 	  this.setAtk(enemyTotalStat/4);
 	  this.setDef(enemyTotalStat/4);
 	  this.setSpeed(enemyTotalStat/4);

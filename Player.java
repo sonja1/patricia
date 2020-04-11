2a192b1676651aca5f6a55999ff5d9be20c7f470
ayylol <davidjgarciao@gmail.com>
2020-04-11 13:57:44 -0600

    working on learnableMoves array, fixing some character methods

--- Player.java
+++ Player.java
@@ -6,33 +6,43 @@
 public class Player extends Character {
   private int xp = 0;
   private int level;
-  private Move[][] possibleMoves = new Move[][];
+  private Move[][] possibleMoves = new Move[4][4];
+  private Move LearnableMove = null;
   
    //Creating the possibleMoves array
   Move punch = new Move("Punch", 70, true, {0,0,-2,0}, 3, 50, 0.9);
-  possibleMoves[3].add(punch);
+  possibleMoves[0][0].add(punch);
   Move kick = new Move("Kick", 80, true, {0,-2,0,0}, 5, 60, 0.8);
-  possibleMoves[3].add(kick);
+  possibleMoves[0][1].add(kick);
   Move slap = new Move("Slap", 50, true, {0,-1,0,0}, 1, 40, 1.0);
-  possibleMoves[3].add(slap);
+  possibleMoves[0][2].add(slap);
   Move dance = new Move("Dance", 10, true, {0,-5,-3,0}, 5, 0, 1.0);
-  possibleMoves[3].add(dance);
-  Move treatYoSelf = new Move("Treat yoself", 70, false, {2,2,2,10}, 5, 0, 1.0);
-  possibleMoves[6].add(treatYoSelf);
+  possibleMoves[0][3].add(dance);
+  Move treatYoSelf = new Move("Treat yoself", 70, false, {2,2,2,10}, 5, 0, 1.0);//heals
+  possibleMoves[1][0].add(treatYoSelf);
   Move fireball = new Move("Fireball", 100, true, {0,-5,0,0}, 2, 70, 1.0);
-  possibleMoves[6].add(fireball);
+  possibleMoves[1][1].add(fireball);
   Move snowball = new Move("Snowball", 100, true, {0,0,-3,0}, 2, 70, 0.95);
-  possibleMoves[6].add(snowball);
+  possibleMoves[1][2].add(snowball);
   Move thunder = new Move("Thunder", 100, true, {0,0,0,0}, 0, 80, 0.95);
-  possibleMoves[6].add(thunder);
-  Move heal = new Move("Heal", 10, false, {5,5,5,30}, 5, 0, 1.0);
-  possibleMoves[9].add(heal);
+  possibleMoves[1][3].add(thunder);
+  Move heal = new Move("Heal", 10, false, {5,5,5,30}, 5, 0, 1.0);//heals
+  possibleMoves[2][0].add(heal);
   Move meteorJam = new Move("Meteor Jam", 130, true, {-2,-2,2,0}, 1, 110, 0.9);
-  possibleMoves[9].add(meteorJam);
+  possibleMoves[2][1].add(meteorJam);
   Move arson = new Move("Arson", 130, true, {0,-4,0,0}, 3, 100, 1.0);
-  possibleMoves[9].add(arson);
+  possibleMoves[2][2].add(arson);
   Move algebra2 = new Move("Algebra II", 130, true, {-2,-3,0,0}, 2, 120, 0.7);
-  possibleMoves[9].add(algebra2);
+  possibleMoves[2][3].add(algebra2);
+  Move heal = new Move("Tactical Brick Throw", 30, true, {-5,0,0,0}, 5, -10, 1.0);
+  possibleMoves[3][0].add(heal);
+  Move meteorJam = new Move("", 130, true, {-2,-2,2,0}, 1, 110, 0.9);
+  possibleMoves[3][1].add(meteorJam);
+  Move arson = new Move("", 130, true, {0,-4,0,0}, 3, 100, 1.0);
+  possibleMoves[3][2].add(arson);
+  Move algebra2 = new Move("", 130, true, {-2,-3,0,0}, 2, 120, 0.7);
+  possibleMoves[3][3].add(algebra2);
+  
   
   //Constructor
   

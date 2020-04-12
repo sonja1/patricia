ab77937a037e55be3c7901232fc54a8cc0e15810
sonjacotra <sonjacotra@sonjas-mbp.hitronhub.home>
2020-04-12 17:38:52 -0600

    fixed enemy constructor, didnt notice attainableitems class at first

--- Enemy.java
+++ Enemy.java
@@ -9,26 +9,8 @@ import java.util.Random;
 public class Enemy extends Character {
   private Items droppable;
   private int tier;
-<<<<<<< HEAD
   private ArrayList<Items> droppableItems = new ArrayList<Items>();
   
-  Items potion = new Items("Potion", 10,true, {0,0,0,20},1,0);
-  droppableItems.add(0,potion);
-  Items superPotion = new Items("Super potion",10,true, {0,0,0,60},1,0);
-  droppableItems.add(5,potion);
-  Items sword = new Items("Sword",10,true, {5,0,-2,0},3,0);
-  droppableItems.add(4,potion);
-  Items shield = new Items("Shield",10,true, {0,5,-2,0},3,0);
-  droppableItems.add(3,potion);
-  Items attackBoost = new Items("Attack booster",10,true, {3,0,0,0},1,0);
-  droppableItems.add(2,potion);
-  Items speed = new Items("Speed",10,true, {0,0,5,0},1,0);
-  droppableItems.add(1,potion);
-  Items key = new Items("Key", 0, false,  {0,0,0,0,}, 1, 0};
-  droppableItems.add(6,potion);
-  
-=======
->>>>>>> branch 'master' of https://github.com/sonja1/patricia.git
   
   //Constructors
    /**
@@ -45,13 +27,9 @@ public class Enemy extends Character {
 	 this.setSpd((1+aTier/10)*aPlayer.getSpd());
 	 this.setMaxHp((1+aTier/10)*aPlayer.getMaxHp());
 	 this.setCurrentHp((1+aTier/10)*aPlayer.getMaxHp());
+	 AttainableItems items = new AttainableItems()
+	 this.droppable = items.getItem();
 	 LearnableMoves moves = new LearnableMoves();
-	 if (aPlayer.getLevel() < 12) {
-		 this.droppable = droppableItems(random.nextInt(6));
-	 }
-	 else {
-		 this.droppable = droppableItems(random.nextInt(4)+3);
-	 }
 	 if (aPlayer.getLevel() < 4) {
 		 while (this.getKnownMoves() < 2) {
 			 this.addMove(moves.getMove(1)) 

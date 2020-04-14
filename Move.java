28f54de6bda4d79b653f511163dba8e45c495859
sonjacotra <sonjacotra@sonjas-mbp.hitronhub.home>
2020-04-14 00:09:30 -0600

    most move javadoc

--- Move.java
+++ Move.java
@@ -44,29 +44,63 @@ public class Move {
 	}
 	
 	//getters and setters
+	/**
+	 * Returns the name of the move.
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * Sets the name of the move.
+	 * @param newName,the string representing the move's new name.
+	 */
 	public void setName(String newName) {
 		name = newName;
 	}
+	
+	/**
+	 * Returns the willpower of the move.
+	 */
 	public int getWillpower() {
 		return willpower;
 	}
+	
+	/**
+	 * Setter for the willpower of a move.
+	 * @param newWillpower, an integer representing the new willpower of a move.
+	 */
 	public void setWillpower(int newWillpower) {
 		willpower = newWillpower;
 	}
+	
+	/**
+	 * Returns the power of a move.
+	 */
 
 	public int getPower() {
 		return power; 
 	}
+	
+	/**
+	 * Setter for the power of a move.
+	 * @param newPower, the integer value of the new power of the move.
+	 */
 	public void setPower(int newPower) {
 		power = newPower;
 	}
-
+	
+	/**
+	 * Returns the accuracy of a move.
+	 */
 	public double getAccuracy() {
 		return accuracy;
 	}
+	
+	/**
+	 * Setter for the accuracy of the move.
+	 * @param newAccuracy, a double value between 0 and 1 representing how accurate a move is.
+	 */
 	public void setAccuracy(double newAccuracy) {
 		if(newAccuracy <= 1 && newAccuracy >= 0) {
 			accuracy = newAccuracy;
@@ -75,28 +109,59 @@ public class Move {
 			accuracy=1.0;
 		}
 	}
+	
+	/**
+	 * Returns the boolean representing whether the enemy is targetted or not.
+	 */
 
 	public boolean getEnemyTargetted() {
 		return enemyTargetted;
 	}
+	
+	/**
+	 * Setter for the boolean of whether the enemy is targetted or not.
+	 * @param the boolean isEnemyTargetted, which will represent if the enemy is targetted or not.
+	 */
 	public void setEnemyTargetted(boolean isEnemyTargetted) {
 		enemyTargetted = isEnemyTargetted;
 	}
-
+	
+	/**
+	 * Returns the stat change array of the move. Each index of the array corresponds to a particular stat.
+	 */
 	public int[] getStatChanges() {
 		return Arrays.copyOfRange(statChanges,0,NUMSTATS);
 	}
+	
+	/**
+	 * Returns the stat change for a particular stat based on the index provided.
+	 * @param the index of the stat in question/
+	 */
 	public int getStatChange(int i) {
 		return statChanges[i];	
 	}
+	
+	/**
+	 * Setter for entire stat change array.
+	 * @param a new statchange array, where each index corresponds to a change to a specific stat.
+	 */
 	public void setStatChanges(int[] newStatChanges) {
 		System.arraycopy(newStatChanges, 0, statChanges, 0, NUMSTATS);
 	}
+	
+	/**
+	 * Setter for a particular stat change.
+	 * @param i, the index of the stat, and change, the integer value of the stat change.
+	 */
 	public void setStatChange(int i, int change) {
 		if(i<NUMSTATS){
 			statChanges[i] = change;
 		}
 	}
+	
+	/**
+	 * 
+	 */
 
 	public void setEffectTurns(int newEffectTurns){
 		if(newEffectTurns >= 0){
